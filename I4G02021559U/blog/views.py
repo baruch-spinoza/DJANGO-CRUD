bfb12600156e8e7d5aac156a9878eadf397ba0cd
baruch-spinoza <ayomideakinwale233@gmail.com>
2022-06-29 16:38:15 +0100

    updated urls blog app

--- I4G02021559U/blog/views.py
+++ I4G02021559U/blog/views.py
@@ -4,7 +4,6 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import PostDeleteView
-import blog
 
 from blog.models import Post
 
