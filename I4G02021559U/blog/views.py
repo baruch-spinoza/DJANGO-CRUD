2555681c3f341b61106d6f7a11fa8ad41732f0e3
baruch-spinoza <ayomideakinwale233@gmail.com>
2022-06-30 15:01:18 +0100

    update urls

--- I4G02021559U/blog/views.py
+++ I4G02021559U/blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
-from django.views.generic.edit import PostDeleteView
+from django.views.generic.edit import DeleteView
 
 from blog.models import Post
 
